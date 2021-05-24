5f05531e986c3ff5ae941e36b958ef74aadef260
Victor <lausellv@yahoo.com>
2021-05-24 17:36:10 +0200

    mounted the routers using middleware

--- app.js
+++ app.js
@@ -11,30 +11,27 @@ app.use(morgan('dev'));
 app.use(express.json()); // middleware is added to express (ie express json)
 
 // we can create our own middleware function
-app.use((req, res, next)=>{
-  console.log('hello from the middleware 😁')
+app.use((req, res, next) => {
+  console.log('hello from the middleware 😁');
   next();
-})
+});
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(`requestedAt: ${req.requestTime}`)
+  console.log(`requestedAt: ${req.requestTime}`);
   next();
-})
+});
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) // no problem running this code here bc it's non-blocking
 );
 
-
 // 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 'success',
-      results: tours.length,
-      data: { tours /*cld be tourNumber . I decide the key value*/: tours },
-    }); // ES6 we don't need to specify key and value if they have the same name
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours /*cld be tourNumber . I decide the key value*/: tours },
+  }); // ES6 we don't need to specify key and value if they have the same name
 };
 
 const createTour = (req, res) => {
@@ -57,25 +54,20 @@ const createTour = (req, res) => {
 const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; // another way of converting a string to a number is to multiply a string by the number 1
-  const tour = tours.find(el => el.id === id)// variables defined in the url are in params
+  const tour = tours.find((el) => el.id === id); // variables defined in the url are in params
 
-// if there is no mathing id const tour wld end up undefined
+  // if there is no mathing id const tour wld end up undefined
   //if (id > tours.length)
-  if (!tour)
-   {
-    return res.status(404).json({status : 'fail',
-    message: `tour ${id} does not exist`
-  })
-  } 
-  res
-    .status(200)
-    .json({ status: 'success', 
-    data: { tour}   // same as tour:tour
-
+  if (!tour) {
+    return res
+      .status(404)
+      .json({ status: 'fail', message: `tour ${id} does not exist` });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour }, // same as tour:tour
   });
 };
-
-
 
 const updateTour = (req, res) => {
   const id = req.params.id * 1; // another way of converting a string to a number is to multiply a string by the number 1
@@ -119,6 +111,36 @@ const deleteTour = (req, res) => {
   );
 };
 
+const getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'this route is not defined yet!' });
+};
+
+const getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'this route is not defined yet!' });
+};
+
+const createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'this route is not defined yet!' });
+};
+
+const updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'this route is not defined yet!' });
+};
+
+const deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'this route is not defined yet!' });
+};
+
 // 3) ROUTES
 
 //app.get('/api/v1/tours', getAllTours);
@@ -127,17 +149,19 @@ const deleteTour = (req, res) => {
 // app.get('/api/v1/tours/:id', getTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
 
-  // 4) SERVER
+app.use('/api/v1/tours', tourRouter);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+app.use('/api/v1/users', userRouter);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+// 4) SERVER
 
 const PORT = 3000;
 app.listen(PORT, '127.0.0.1', () => {

bc7d881870172c8950416d859da3e6016e18c781
Victor Lausell <lausellv@yahoo.com>
2021-05-27 18:05:40 +0200

    addedd console log NODE.ENV

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'})
 const app = require('./app');
 
-dotenv.config({path:'./config.env'})
-console.log(process.env);
-const PORT = 3000;
+// console.log(process.env);
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`listening on port ${PORT}...`);
 });

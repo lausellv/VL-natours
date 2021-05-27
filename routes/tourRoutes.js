6c6308db9c24d857a25ccea5c9b613199726a91d
Victor Lausell <lausellv@yahoo.com>
2021-05-27 17:14:34 +0200

    added checkID tour handler

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,9 @@ const tourController = require('./../controllers/tourControllers')
 
 const router = express.Router();
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+//this is part of the middleware stack of the tourRouter 'app'
+router.param('id', tourController.checkID);
+router.route('/').get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router; 

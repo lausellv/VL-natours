b266a899779b54e918d3f8e201db9b061c557d8a
Victor Lausell <lausellv@yahoo.com>
2021-05-27 17:49:32 +0200

    dotenv.config

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const app = require ('./app');
+const dotenv = require('dotenv');
+const app = require('./app');
 
+dotenv.config({path:'./config.env'})
+console.log(process.env);
 const PORT = 3000;
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`listening on port ${PORT}...`);

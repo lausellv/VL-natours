ae9f4eee5d3b6863bf74b87d3403447cd91a67af
Victor <lausellv@yahoo.com>
2021-05-28 13:07:02 +0200

    added eslint packages

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+/* eslint-disable prettier/prettier */
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'})
-const app = require('./app');
-//console.log(app.get('env')) // environment variables are global variables that define the environment where the node app is running.
-console.log(process.env) // node.js enviroment variables 
+
+dotenv.config({ path: './config.env' }); // this has to be read first
+const app = require('./app'); // this has to be read second
+
+console.log(app.get('env')); // environment variables are global variables that define the environment where the node app is running.
+//console.log(process.env) // node.js enviroment variables
 
 // console.log(process.env);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`listening on port ${PORT}...`);
 });
+
+

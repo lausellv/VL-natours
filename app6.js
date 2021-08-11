388eaed85704f3f4910b15f8b64ee6820d463391
Victor <lausellv@yahoo.com>
2021-08-11 13:32:10 +0200

    66- serving static files

--- app6.js
+++ app6.js
@@ -7,6 +7,7 @@ const userRouter = require(`${__dirname}/routes/userRoutes2`);
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('hello from the middleware 😀');
   next();

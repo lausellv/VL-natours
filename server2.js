9f0b22e9678cd301394c988cff36161b2afbff7c
Victor <lausellv@yahoo.com>
2021-08-29 16:57:27 +0200

    lesson 85-mongoose and model schema

--- server2.js
+++ server2.js
@@ -1,14 +1,53 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({path: `${__dirname}/config.env`})  // this needs to be declared before app
+dotenv.config({ path: `${__dirname}/config.env` }); // this needs to be declared before app
 const app = require('./app6');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connnection succesful!');
+  });
 
-console.log(process.env.NODE_ENV); // eslint-disable-line 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please write a tour name'],
+    unique: true
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'tour must include price'] }
+});
 
-const PORT = (process.env.PORT || 3000);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Campers',
+  price: 499
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`ERROR 🛠: ${err}`);
+  });
+
+console.log(process.env.NODE_ENV); // eslint-disable-line
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`); // eslint-disable-line 
+  console.log(`app running on port ${PORT}`); // eslint-disable-line
 });
